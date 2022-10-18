a386f28d9ee6569e750ff484de9a99c66f9ebb05
Dominos0321 <dsch792@gmail.com>
2022-10-18 09:33:29 +0200

    Not Initial Commit

--- app/src/main/java/com/example/niezbytmobilnaaplikacja/MainActivity.kt
+++ app/src/main/java/com/example/niezbytmobilnaaplikacja/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.niezbytmobilnaaplikacja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val a = findViewById<EditText>(R.id.editTextTextPersonName)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.imie).text = findViewById<TextView>(R.id.imie).text.toString() + a.toString()
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            findViewById<TextView>(R.id.imie).text = "Jakub Niemczyk"
+        }
     }
 }
